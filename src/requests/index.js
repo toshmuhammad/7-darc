3c3d7e03126d8df5ba41ed8e03f20b97a3257ccc
toshmuhammad <toshmuhammad@gmail.com>
2025-05-05 07:09:55 +0500

    1

--- src/requests/index.js
+++ src/requests/index.js
@@ -1,9 +1,21 @@
 export async function getData(query = "") {
-    const req = await fetch(`https://json-api.uz/api/project/fn37/travels?${query}`);
-    if (req.status === 200) {
-        const result = await req.json();
-        return result.data;
-    } else {
-        throw new Error("Xatolik");
+    try {
+        const res = await fetch(`https://json-api.uz/api/project/fn37/travels?${query}`);
+        if (!res.ok) throw new Error(`HTTP xatolik: ${res.status}`);
+        const json = await res.json();
+        return json.data;
+    } catch (err) {
+        console.error("Ma'lumot olishda xatolik:", err.message);
+        return null;
     }
 }
+
+// export async function getData(query = "") {
+//     const req = await fetch(`https://json-api.uz/api/project/fn37/travels?${query}`);
+//     if (req.status === 200) {
+//         const result = await req.json();
+//         return result.data;
+//     } else {
+//         throw new Error("Xatolik");
+//     }
+// }
